d22b6dc568f1abb31e9a50868876a522e64ad991
wins1978 <2071227188@qq.com>
2019-06-18 13:04:56 +0800

    D

--- mq_historical_data_collection/settings.py
+++ mq_historical_data_collection/settings.py
@@ -4,7 +4,7 @@ from playhouse.db_url import connect
 
 def init():
     global db 
-    db= connect(os.environ.get('MY_DATABASE')) 
+    db= connect(os.environ.get('PROD_DATABASE')) 
     
     global byday_data_list
     byday_data_list = []
